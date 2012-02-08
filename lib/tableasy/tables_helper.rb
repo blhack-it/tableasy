dbfee74218f5433a42d643a0945302a24145e735
Simone Lovati <lovati@advboucle.com>
2012-02-08 12:00:03 +0100

    Removed unused helpers

--- lib/tableasy/tables_helper.rb
+++ lib/tableasy/tables_helper.rb
@@ -1,40 +1,15 @@
 module Tableasy
   module TablesHelper
-    def table_for(klass, list, *columns)
-      options = columns.extract_options!
 
-      table = Table.new
-      # Add header row
-      row = columns.collect {|column| header_cell(column, klass) }
-      table.add_row(row, :header => true)
-
-      list.each_with_index do |object, index|
-        table.add_row(table_row(object, columns), :id => dom_id(object, :row), :class => "#{dom_class(object)} #{index.even? ? "odd" : "even"}")
-      end
-
-      if options[:total]
-        item = Total.new(list)
-        caption = I18n.t('tableasy.total', :raise => true) rescue 'Total: '
-        table.add_row(table_row(item, columns[1..-1]).unshift(Table::Cell.new(item, caption, true)), :class => 'total-row', :total => true)
-      end
-
+    def render_table(table, options={})
       content_tag('table', options[:html]) do
         table.rows[1..-1].each {|row| yield row } if block_given?
         table.rows.each {|row| concat content_row(row) }
       end
     end
 
-    def data_list(object, *columns)
-      options = columns.extract_options!
-      table = Table.new
-
-      columns.each do |column|
-        table.add_row([header_cell(column, object.class), Table::Cell.new(object, column)].compact)
-      end
-
-      content_tag('table', options[:html]) do
-        table.rows.each {|row| concat content_row(row) }
-      end
+    def table_cell(value, header=false)
+      Tableasy::Table::Cell.new(nil, value.to_s.html_safe, header)
     end
 
     def content_row(row)
